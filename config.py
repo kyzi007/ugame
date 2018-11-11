9394a2a32a3a1b0ba2feb5f630fe36dcede90dd8
el.kuznetsova <el.kuznetsova@lucky-labs.com>
2018-11-11 19:54:06 +0200

    work!

--- config.py
+++ config.py
@@ -1,12 +1,123 @@
 # -*- coding:utf-8 -*-
 
-MAP_SIZE = 10
-VISIBLE_MAP_SIZE = 3
-BOTS_COUNT = 10
-# MAP_SIZE = 512
+# MAP_SIZE = 40
 # VISIBLE_MAP_SIZE = 32
-# BOTS_COUNT = 20000
+# BOTS_COUNT = 100
+MAP_SIZE = 512
+VISIBLE_MAP_SIZE = 32
+BOTS_COUNT = 20000
 
 MOVE_SPEED = 5
 
 SECRET = b'CRYEIwWU+seFwNtEaMa4qWx8VCJbRUVv'
+
+NAMES = '''Emma
+Olivia
+Sophia
+Isabella
+Ava
+Mia
+Emily
+Abigail
+Madison
+Charlotte
+Harper
+Sofia
+Avery
+Elizabeth
+Amelia
+Evelyn
+Ella
+Chloe
+Victoria
+Aubrey
+Grace
+Zoey
+Natalie
+Addison
+Lillian
+Brooklyn
+Lily
+Hannah
+Layla
+Scarlett
+Aria
+Zoe
+Samantha
+Anna
+Leah
+Audrey
+Ariana
+Allison
+Savannah
+Arianna
+Camila
+Penelope
+Gabriella
+Claire
+Aaliyah
+Sadie
+Riley
+Skylar
+Nora
+Sarah
+Hailey
+Kaylee
+Paisley
+Kennedy
+Ellie
+Peyton
+Annabelle
+Caroline
+Madelyn
+Serenity
+Aubree
+Lucy
+Alexa
+Alexis
+Nevaeh
+Stella
+Violet
+Genesis
+Mackenzie
+Bella
+Autumn
+Mila
+Kylie
+Maya
+Piper
+Alyssa
+Taylor
+Eleanor
+Melanie
+Naomi
+Faith
+Eva
+Katherine
+Lydia
+Brianna
+Julia
+Ashley
+Khloe
+Madeline
+Ruby
+Sophie
+Alexandra
+London
+Lauren
+Gianna
+Isabelle
+Alice
+Vivian
+Hadley
+Jasmine
+Morgan
+Kayla
+Cora
+Bailey
+Kimberly
+Reagan
+Hazel
+Clara
+Sydney
+Trinity'''.split('\n')
--- server.py
+++ server.py
@@ -2,9 +2,7 @@
 import json
 import random
 
-import time
 import redis
-import asyncio_redis
 
 from aiohttp import web
 from aiohttp_session import get_session, setup
@@ -14,50 +12,9 @@ import config
 routes = web.RouteTableDef()
 
 
-def get_redis():
+def get_redis_from_pool():
     pool = redis.ConnectionPool(host='localhost', port=6379)
     return redis.Redis(connection_pool=pool)
-
-
-def generate_bots(count, map_size):
-    bots = random.sample(range(0, map_size * map_size), count)
-    db = get_redis()
-    db.sadd('cells', *bots)
-
-
-def take_free_cell(map_size):
-    db = get_redis()
-    cells_total = map_size * map_size
-
-    players_count = db.scard('cell')
-    if players_count > cells_total / 4:
-        return -1
-
-    while True:
-        cell = random.randint(0, cells_total)
-        exist = db.sismember('cells', cell)
-
-        if not exist:
-            db.sadd('cells', cell)
-            return cell
-
-
-def get_map_area(center_point, area_map_size, map_size):
-    db = get_redis()
-    cells = db.smembers('cells')
-    cells = map(lambda x: int(x), cells)
-
-    # area left top cell
-    start = center_point - map_size * int(area_map_size / 2)
-    start -= int(area_map_size / 2)
-    start = max(0, start)
-
-    view_cells = []
-    for i in range(0, area_map_size):
-        view_cells += filter(lambda x: start + map_size * i <= x < start + map_size * i + area_map_size, cells)
-
-    return view_cells, start
-
 
 
 def to_xy(cell: int, map_size: int):
@@ -66,37 +23,132 @@ def to_xy(cell: int, map_size: int):
     return x, y
 
 
+def generate_bot_info(uid):
+    return {
+        'player:%s:type' % uid: 'bot',
+        'player:%s:name' % uid: 'Bot %s' % random.choice(config.NAMES)
+    }
+
+
+def generate_player_info(uid):
+    return {
+        'player:%s:type' % uid: 'player',
+        'player:%s:name' % uid: 'Player %s' % random.choice(config.NAMES)
+    }
+
+
+def generate_bots(db, total, map_size):
+    """
+    Create and save bots
+    """
+    bots = random.sample(range(0, map_size * map_size), total)
+    db.sadd('cells', *bots)
+    for uid in bots:
+        info = generate_bot_info(uid)
+        for key, value in info.items():
+            db.append(key, value)
+
+
+def search_free_cell(db, map_size):
+    """
+    Search free cell for player
+    """
+    cells_total = map_size * map_size
+
+    players_count = db.scard('cell')
+    # limit players in map
+    if players_count > cells_total / 2:
+        return -1
+
+    while True:
+        cell = random.randint(0, cells_total)
+        exist = db.sismember('cells', cell)
+
+        if not exist:
+            return cell
+
+
+def cut_map_area(all_cells, center_point, area_map_size, map_size):
+    view_cells = []
+    all_cells = map(lambda x: int(x), all_cells)
+
+    # area left top cell
+    start = center_point - map_size * int(area_map_size / 2)
+    start -= int(area_map_size / 2)
+    start = max(0, start)
+
+    all_cells = list(all_cells)
+    for i in range(0, area_map_size):
+        start_ = start + map_size * i
+        view_cells += filter(lambda x: start_ <= x < start_ + area_map_size, all_cells)
+
+    return view_cells, start
+
+
 @routes.get('/game')
 async def game(request):
+    """
+    init and save player, save player uid into session and return map
+    """
     session = await get_session(request)
+    # not async redis a lot faster
+    # one point to db access
+    db = get_redis_from_pool()
 
     if session.new:
-        session['cell'] = take_free_cell(config.MAP_SIZE)
+        cell = session['cell'] = search_free_cell(db, config.MAP_SIZE)
+        if cell == -1:
+            response_data = {'error': 'no free cells'}
+            return web.Response(body=json.dumps(response_data))
 
-    if session['cell'] == -1:
-        response_data = {'error': 'no free cells'}
+        db.sadd('cells', cell)
+
+        # generate random info
+        player_info = generate_player_info(cell)
+        for key, value in player_info.items():
+            db.append(key, value)
     else:
-        cells, start = get_map_area(
-            center_point=session['cell'],
-            area_map_size=config.VISIBLE_MAP_SIZE,
-            map_size=config.MAP_SIZE)
+        cell = session['cell']
 
-        response_data = {
-            'cell': to_xy(session['cell'], config.MAP_SIZE),
-            'map': list(map(lambda cell: to_xy(cell, config.MAP_SIZE), cells)),
-            'map_start': to_xy(start, config.MAP_SIZE),
-            'view_width': config.VISIBLE_MAP_SIZE,
-            'view_height': config.VISIBLE_MAP_SIZE,
-            'map_width': config.MAP_SIZE,
-            'map_height': config.MAP_SIZE
-        }
+    all_cells = db.smembers('cells')
+
+    cells, start = cut_map_area(
+        all_cells=all_cells,
+        center_point=cell,
+        area_map_size=config.VISIBLE_MAP_SIZE,
+        map_size=config.MAP_SIZE)
+    map_info = []
+
+    # gets bots and other players info
+    for uid in cells:
+        map_info.append(
+            {
+                # todo tasks
+                'name': db.get('player:%s:name' % uid),
+                'type': db.get('player:%s:type' % uid),
+                'uid': uid,
+                'coord': to_xy(uid)
+            }
+        )
+
+    response_data = {
+        # i don`t hate frontend developers
+        'player_uid': cells,
+        'player_coord': to_xy(cell, config.MAP_SIZE),
+        'map': list(map(lambda x: to_xy(x, config.MAP_SIZE), cells)),
+        'map_start': to_xy(start, config.MAP_SIZE),
+        'view_width': config.VISIBLE_MAP_SIZE,
+        'view_height': config.VISIBLE_MAP_SIZE,
+        'map_width': config.MAP_SIZE,
+        'map_height': config.MAP_SIZE
+    }
 
     return web.Response(body=json.dumps(response_data))
 
 
 class Game(web.Application):
     async def startup(self) -> None:
-        db = get_redis()
+        db = get_redis_from_pool()
         db.flushdb()
         generate_bots(config.BOTS_COUNT, config.MAP_SIZE)
         await self.on_startup.send(self)
